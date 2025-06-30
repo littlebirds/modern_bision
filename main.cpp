d568311aa2737e579836c14d892a1b172997ab5d
Gang Zhi <gangzhi2000@gmail.com>
2025-06-29 23:23:25 -0400

    scalfold

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "fstream"
+#include "Scanner.hpp"
+#include "Parser.hpp"
+
+
+int main() {
+    std::ifstream inputFile; // Declare the ifstream object
+    inputFile.open("input.txt"); // 
+    monkey::Scanner scanner{inputFile , std::cerr };
+    monkey::Parser parser{ &scanner };
+    std::cout.precision(10);
+    parser.parse();
+}
